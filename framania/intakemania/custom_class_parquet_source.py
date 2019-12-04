4cb1eaed7cf25a0f4ec8815ad1d224b4480d41be
higumachan <harekumo792154@gmail.com>
2019-12-04 21:36:27 +0900

    自分で定義したclassをintakeでuploadしたりloadしたりできるようにする

---
+++ framania/intakemania/custom_class_parquet_source.py
@@ -0,0 +1,46 @@
+import pickle
+from base64 import b64decode, b64encode
+from pathlib import Path
+from typing import List, Union
+
+import intake
+import pandas
+from intake_parquet import ParquetSource
+import dask.dataframe as dd
+
+
+# TODO(higumachan): のちのちに辛くなってくる可能性があるので可能ならばJSONで書き出したりができるようにしておきたい
+def _dump_with_pickle(obj):
+    s = pickle.dumps(obj)
+    return b64encode(s).decode("ascii")
+
+
+def _load_with_pickle(pickle_str: str):
+    b = b64decode(pickle_str.encode("ascii"))
+    return pickle.loads(b)
+
+
+class CustomClassParquetSource(ParquetSource):
+    def to_spark(self):
+        raise NotImplementedError("only use with dask")
+
+    def _to_dask(self):
+        super()._to_dask()
+        for col in self.metadata["custom_class_columns"]:
+            self._df[col] = self._df[col].map(_load_with_pickle, meta=dd.utils.make_meta(self._df[col]))
+        return self._df
+
+    @classmethod
+    def from_parqeuet_source(cls, ps: ParquetSource):
+        ccps = cls.__new__(cls)
+        ccps.__dict__ = ps.__dict__
+        return ccps
+
+
+def upload_with_custom_class(df: dd.DataFrame, path: Union[str, Path], custom_class_cols: List[str], **kwargs):
+    df = df.copy()
+    for col in custom_class_cols:
+        df[col] = df[col].map(_dump_with_pickle, meta=dd.utils.make_meta(df[col]))
+    parquet_source: ParquetSource = intake.upload(df, str(path), **kwargs)
+    parquet_source.metadata.update({"custom_class_columns": custom_class_cols})
+    return CustomClassParquetSource.from_parqeuet_source(parquet_source)
---
+++ test/test_intakemania_custom_class_paquet_source.py
@@ -0,0 +1,62 @@
+from tempfile import mkdtemp, mktemp
+from unittest import TestCase
+
+import intake
+import pandas
+from dask.dataframe.utils import make_meta
+from intake.catalog.local import YAMLFileCatalog
+from intake.source.csv import CSVSource
+
+from framania.intakemania.custom_class_parquet_source import upload_with_custom_class, CustomClassParquetSource
+from framania.pandasmania.util import md5hash
+
+
+class MyClass:
+    def __init__(self, x):
+        self._x = x
+
+    def __eq__(self, other):
+        return self._x == other._x
+
+    def __hash__(self):
+        return hash(self._x)
+
+
+class TestFramaniaExtendedIntake(TestCase):
+    def setUp(self) -> None:
+        self.csvsource1 = CSVSource(urlpath='test/data/test-csv1.csv')
+
+    @staticmethod
+    def create_custom_source(df):
+        filename = mktemp()
+        df["d"] = df["b"].map(lambda x: MyClass(x), meta=make_meta(df["b"]))
+        source = upload_with_custom_class(df, filename, ["d"])
+
+        return source, filename
+
+    def test_upload_with_custom_class(self):
+        df = self.csvsource1.to_dask()
+        source, _ = self.create_custom_source(df)
+        assert source.metadata["custom_class_columns"] == ["d"]
+        assert df["d"].head().iloc[0] == MyClass("a")
+        assert isinstance(source, CustomClassParquetSource)
+
+    def test_load_with_custom_class(self):
+        df = self.csvsource1.to_dask()
+        source, _ = self.create_custom_source(df)
+        yaml_file = mktemp(suffix=".yaml")
+        f = open(yaml_file, "w")
+        f.write("""\
+metadata:
+  version: 1
+sources: {}""")
+        f.close()
+        cat = intake.open_catalog(yaml_file)
+        cat.add(source, name="test")
+
+        df2 = source.to_dask()
+
+        assert all(df["d"].compute() == df2["d"].compute())
+        assert all(df.compute() == df2.compute())
+        # TODO(higumachan): これが通らない
+        # assert md5hash(df.compute()) == md5hash(df2.compute())
